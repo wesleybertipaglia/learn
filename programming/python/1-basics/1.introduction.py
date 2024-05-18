5c6946f905963bbfed27346c9075d4f9ba1aa7f5
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-05-18 07:08:17 -0300

    reafactor: update some comments in introduction.py

--- programming/python/1-basics/1.introduction.py
+++ programming/python/1-basics/1.introduction.py
@@ -1,22 +1,22 @@
 # Basics of Python
+import array
 
 # variables and data types
 x = 10 # Integers
 y = 3.14 # Floats
+major_age = True # Booleans
 name = "Alice" # Strings
 numbers = [1, 2, 3, 4, 5] # Lists
+ages = array(18, 34, 25, 19) # Arrays
 coordinates = (10, 20) # Tuples
 person = {"name": "Bob", "age": 30} # Dictionaries
-my_set = {"apple", "banana", "cherry"} # Sets
-my_bool = True
+fruits = {"apple", "banana", "cherry"} # Sets
 
-# comment 
+# comments
 
-# output
-print("Hello, world!")
-
-# input 
+# input and output
 username = input("Enter username:")
+print("Hello, world!")
 
 # function
 def hello_world(): 
@@ -24,12 +24,14 @@ def hello_world():
 	
 def hello_name(name): # parameters
 	print("Hello, ", name)
-	
-sum = lambda a,b: a + b # lambda (one line function)
 
 hello_name("wesley") # calling a function
 
-# operators
+# lambda (one line function)
+sum = lambda a,b: a + b
+print(sum(1, 2)) # 3
+
+# arithmetic operators
 sum = 1 + 2
 sub = 5 - 3
 mul = 8 * 5
@@ -40,7 +42,6 @@ exp = 2 ** 3
 
 # conditionals
 value = 20
-
 if value < 10:
     print("the value is less to 10")
 elif value == 10:
@@ -50,7 +51,6 @@ else:
 
 # while loop
 i = 0
-
 while i < 10:
     if i == 5:
         break # stop the loop
@@ -64,6 +64,10 @@ while i < 10:
 fruits = ['apple', 'banana', 'kwi', 'coconut']
 
 for fruit in fruits:
+    if fruit == "banana":
+        break # break the entry loop
+    if fruit == "coconut":
+        continue # skip the current interation
     print(fruit)
 
 # for in range
