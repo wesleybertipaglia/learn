ebbb31a7df047d0bedd2fd3d2d6962be665c14f2
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-05-18 09:06:09 -0300

    refactor: Rename binary-search.py to binary_search.py

--- programming/python/6-algorithms/binary_search.py
+++ programming/python/6-algorithms/binary_search.py
@@ -11,8 +11,4 @@ def binary_search(items, target):
             end = mid - 1
         else:
             start = mid + 1
-    return None
-
-
-numbers = [1,3,5,7,8,9,11,13,15,16,17,20,26,28,35,38,39,40]
-print(binary_search(numbers, 20))
+    return -1
